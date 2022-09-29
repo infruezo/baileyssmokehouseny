a674fd04bc90430a2ce5b796135abfc3d83f340c
belghoul <belghoul@systemd.se>
2022-09-29 15:45:23 +0100

    Fixed time display on smokehouse's events in home page

--- src/components/smokehouse/EventCard.jsx
+++ src/components/smokehouse/EventCard.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-// import { format, parseISO } from "date-fns";
+import { format, parseISO } from "date-fns";
 
 const EventCard = ({ event }) => {
+  console.log(format(new Date(event.event.startDate), "MMMM dd, yyyy"));
+
   return (
     <div className="w-full col-span-1 h-96 shadow-lg rounded-lg overflow-hidden ring-1 ring-primary-smokehouseBrown/10  hover:ring-primary-smokehouseBrown duration-300 cursor-pointer">
       <img
@@ -14,8 +16,8 @@ const EventCard = ({ event }) => {
       <div className="flex flex-col space-y-1 p-4">
         <h5 className="font-medium">{event?.event.title}</h5>
         <h5 className="font-bold text-primary-smokehouseBrown">
-          {event.event.startDate.toString()}
-          {/* {format(parseISO(event.event.startDate.toString()), "MMMM dd, yyyy")} */}
+          {format(new Date(event.event.startDate), "MMMM dd, yyyy")}
+          {/* {format(parseISO(new Date(event.event.startDate)), "MMMM dd, yyyy")} */}
         </h5>
       </div>
     </div>
--- src/utils/eventData.js
+++ src/utils/eventData.js
@@ -7,7 +7,7 @@ export const SmokehouseEvents = [
     text: "",
     startDate: removeTime(new Date(2022, 0, 1)),
     endDate: removeTime(new Date(2022, 11, 31)),
-    days: [5]
+    days: [5],
   },
   {
     image: "/events/Dead_Mile_Dance.jpeg",
@@ -16,7 +16,7 @@ export const SmokehouseEvents = [
     text: "",
     startDate: removeTime(new Date(2022, 8, 30)),
     endDate: removeTime(new Date(2022, 8, 30)),
-    days: [5]
+    days: [5],
   },
   {
     image: "/events/Kickin_Nash.jpeg",
@@ -25,7 +25,7 @@ export const SmokehouseEvents = [
     text: "",
     startDate: removeTime(new Date(2022, 9, 1)),
     endDate: removeTime(new Date(2022, 9, 1)),
-    days: [6]
+    days: [6],
   },
   {
     image: "/events/Oktoberfest.jpeg",
@@ -34,16 +34,12 @@ export const SmokehouseEvents = [
     text: "",
     startDate: removeTime(new Date(2022, 8, 29)),
     endDate: removeTime(new Date(2022, 9, 2)),
-    days: [0, 4, 5, 6]
+    days: [0, 4, 5, 6],
   },
 ];
 
 function removeTime(date = new Date()) {
-  return new Date(
-    date.getFullYear(),
-    date.getMonth(),
-    date.getDate()
-  );
+  return new Date(date.getFullYear(), date.getMonth(), date.getDate());
 }
 
 export function getUpcomingEvents(num) {
@@ -53,17 +49,23 @@ function getUpcomingEventsInternal(eventList, num) {
   var matchedEvents = [];
   let d = removeTime(new Date());
 
-  const maxDate = new Date(Math.max(...eventList.map(e => { return new Date(e.endDate); }),),);
+  const maxDate = new Date(
+    Math.max(
+      ...eventList.map((e) => {
+        return new Date(e.endDate);
+      })
+    )
+  );
 
   do {
-    eventList.forEach(e => {
+    eventList.forEach((e) => {
       if (d >= e.startDate && d <= e.endDate && e.days.includes(d.getDay())) {
         matchedEvents.push({ date: new Date(d), event: e });
       }
     });
 
-    d.setDate(d.getDate() + 1)
-  } while (matchedEvents.length < num && d <= maxDate)
+    d.setDate(d.getDate() + 1);
+  } while (matchedEvents.length < num && d <= maxDate);
 
   return matchedEvents.slice(0, num);
 }
@@ -76,14 +78,14 @@ function getEventsForMonthInternal(eventList, year, month) {
   let d = removeTime(new Date(year, month, 1));
 
   do {
-    eventList.forEach(e => {
+    eventList.forEach((e) => {
       if (d >= e.startDate && d <= e.endDate && e.days.includes(d.getDay())) {
         matchedEvents.push({ date: new Date(d), event: e });
       }
     });
 
-    d.setDate(d.getDate() + 1)
-  } while (d.getMonth() === month)
+    d.setDate(d.getDate() + 1);
+  } while (d.getMonth() === month);
 
   return matchedEvents;
 }
