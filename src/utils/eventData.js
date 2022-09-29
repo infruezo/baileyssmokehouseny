7ab1ba9159245189e451a8d98606e948db6af1f5
Craig Finch <CFinch@nextgen.com>
2022-09-28 21:53:15 -0400

    event data local functions

---
+++ src/utils/eventData.js
@@ -0,0 +1,89 @@
+//days 0-6, starting sunday
+export const SmokehouseEvents = [
+  {
+    image: "/events/JP_Laramee.jpg",
+    title: "Magical Fridays with JP Laramee",
+    category: "Entertainment",
+    text: "",
+    startDate: removeTime(new Date(2022, 0, 1)),
+    endDate: removeTime(new Date(2022, 11, 31)),
+    days: [5]
+  },
+  {
+    image: "/events/Dead_Mile_Dance.jpeg",
+    title: "Dead Mile Dance",
+    category: "Live Music",
+    text: "",
+    startDate: removeTime(new Date(2022, 8, 30)),
+    endDate: removeTime(new Date(2022, 8, 30)),
+    days: [5]
+  },
+  {
+    image: "/events/Kickin_Nash.jpeg",
+    title: "Kickin Nash",
+    category: "Live Music",
+    text: "",
+    startDate: removeTime(new Date(2022, 9, 1)),
+    endDate: removeTime(new Date(2022, 9, 1)),
+    days: [6]
+  },
+  {
+    image: "/events/Oktoberfest.jpeg",
+    title: "Oktoberfest",
+    category: "Entertainment",
+    text: "",
+    startDate: removeTime(new Date(2022, 8, 29)),
+    endDate: removeTime(new Date(2022, 9, 2)),
+    days: [0, 4, 5, 6]
+  },
+];
+
+function removeTime(date = new Date()) {
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate()
+  );
+}
+
+export function getUpcomingEvents(num) {
+  return getUpcomingEventsInternal(SmokehouseEvents, num);
+}
+function getUpcomingEventsInternal(eventList, num) {
+  var matchedEvents = [];
+  let d = removeTime(new Date());
+
+  const maxDate = new Date(Math.max(...eventList.map(e => { return new Date(e.endDate); }),),);
+
+  do {
+    eventList.forEach(e => {
+      if (d >= e.startDate && d <= e.endDate && e.days.includes(d.getDay())) {
+        matchedEvents.push({ date: new Date(d), event: e });
+      }
+    });
+
+    d.setDate(d.getDate() + 1)
+  } while (matchedEvents.length < num && d <= maxDate)
+
+  return matchedEvents.slice(0, num);
+}
+
+export function getEventsForMonth(year, month) {
+  return getEventsForMonthInternal(SmokehouseEvents, year, month);
+}
+function getEventsForMonthInternal(eventList, year, month) {
+  var matchedEvents = [];
+  let d = removeTime(new Date(year, month, 1));
+
+  do {
+    eventList.forEach(e => {
+      if (d >= e.startDate && d <= e.endDate && e.days.includes(d.getDay())) {
+        matchedEvents.push({ date: new Date(d), event: e });
+      }
+    });
+
+    d.setDate(d.getDate() + 1)
+  } while (d.getMonth() === month)
+
+  return matchedEvents;
+}
